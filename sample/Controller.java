c781078a0af1077a92d87eb8cfaf214c2f4f77f5
SilverSmoke <belycov@rambler.ru>
2018-02-25 22:13:03 +0600

    Second commit and push

--- sample/Controller.java
+++ sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    public Button digit0;
+    public Button equally;
+    public Button digit9;
+    public Button point;
+    public Button digit8;
+    public Button digit7;
+    public Button division;
+    public Button multiply;
+    public Button digit6;
+    public Button digit5;
+    public Button minus;
+    public Button digit3;
+    public Button digit2;
+    public Button digit1;
+    public Button digit4;
+    public Button plus;
+    public Button sqr;
+    public Button percent;
+    public Button clear;
+    public Button three0;
+
+    public void click(MouseEvent mouseEvent) {
+    }
 }
